aefc63fc2bfcf612a9b2d32cf096e2114555c64d
SoengSaravit <soengsaravit@gmail.com>
2017-08-05 08:22:25 +0700

    Initial Commit

---
+++ Localization-05-08-2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Localization-05-08-2017
+//
+//  Created by Soeng Saravit on 8/5/17.
+//  Copyright © 2017 Soeng Saravit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
